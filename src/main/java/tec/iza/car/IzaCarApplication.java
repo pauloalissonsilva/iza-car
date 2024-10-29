a32738b1288cbd4ff62e244a011745af7ac74e25
paulo alisson silva costa <99668631+pauloalissonsilva@users.noreply.github.com>
2024-10-29 19:50:27 -0300

    Initial commit

---
+++ src/main/java/tec/iza/car/IzaCarApplication.java
@@ -0,0 +1,13 @@
+package tec.iza.car;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IzaCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IzaCarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tec/iza/car/IzaCarApplicationTests.java
@@ -0,0 +1,13 @@
+package tec.iza.car;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IzaCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
